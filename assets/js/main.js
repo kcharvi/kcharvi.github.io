6c36b4edd1c47f537e01bd7215a126d0f6d08da5
K CHARVI <kcharvi01@gmail.com>
2024-06-09 17:51:02 +0530

    CSS update

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,32 +162,26 @@ modalCloses.forEach((modalClose) => {
 });
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
-    cssMode: true,
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
     loop: true,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-/*==================== ARTICLES ====================*/
-let swiperarticles = new Swiper(".articles__container", {
-    loop: true,
-    grapCursor: true,
-    spaceBetween: 40,
-
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     breakpoints: {
         568: {
             slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
         },
     },
 });
